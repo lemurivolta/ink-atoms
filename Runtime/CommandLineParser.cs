c03fdb66f8a929fb147f37b41400166dea17e1c8
Mattia Belletti <mbelletti@greenteam.it>
2024-05-17 22:14:20 +0200

    feat: uniform interfaces for command line processor, tag processor, external function

--- Runtime/CommandLineParser.cs
+++ Runtime/CommandLineParser.cs
@@ -48,28 +48,6 @@ namespace LemuRivolta.InkAtoms
         /// <param name="parameters">Parameters passed to the command</param>
         /// <param name="choices">The choices attached to this command.</param>
         /// <param name="commandLineParserAction">The action to perform after this command line result has been processed. By default, it continues.</param>
-        public abstract IEnumerator Invoke(IDictionary<string, Parameter> parameters,
-            StoryChoice[] choices,
-            CommandLineParserAction commandLineParserAction);
-
-        /// <summary>
-        /// Checks that the given parameters set contains a certain parameter.
-        /// If it does not, throws an exception.
-        /// </summary>
-        /// <param name="parameters">The parameters passed to the command.</param>
-        /// <param name="parameterName">The parameter to check.</param>
-        /// <returns>The parameter found</returns>
-        protected string GetParameter(
-            IDictionary<string, Parameter> parameters,
-            string parameterName)
-        {
-            if (!parameters.TryGetValue(parameterName, out var parameter))
-            {
-                throw new System.ArgumentException(
-                    $"{CommandName} must contain a parameter \"{parameterName}:...\"",
-                    "parameters");
-            }
-            return parameter.Value;
-        }
+        public abstract IEnumerator Process(CommandLineParserContext context);
     }
 }
