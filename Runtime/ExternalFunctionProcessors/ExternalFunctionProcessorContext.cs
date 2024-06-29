f60263342877871b1b6d60b9b2cbd79f7fefc9d2
mattia <mattia.belletti@gmail.com>
2024-06-29 14:58:52 +0200

    feat: external functions errors

--- Runtime/ExternalFunctionProcessors/ExternalFunctionProcessorContext.cs
+++ Runtime/ExternalFunctionProcessors/ExternalFunctionProcessorContext.cs
@@ -3,27 +3,7 @@ using System.Runtime.Serialization;
 
 namespace LemuRivolta.InkAtoms.ExternalFunctionProcessors
 {
-    [Serializable]
-    public class UnmanageableAsyncSequenceException : Exception
-    {
-        public UnmanageableAsyncSequenceException()
-        {
-        }
 
-        public UnmanageableAsyncSequenceException(string message) : base(message)
-        {
-        }
-
-        public UnmanageableAsyncSequenceException(string message, Exception inner) : base(message, inner)
-        {
-        }
-
-        protected UnmanageableAsyncSequenceException(
-            SerializationInfo info,
-            StreamingContext context) : base(info, context)
-        {
-        }
-    }
 
     /// <summary>
     ///     Base class for <see cref="ExternalFunctionProcessorContextWithResult" /> that doesn't wrap the result.
@@ -36,39 +16,5 @@ namespace LemuRivolta.InkAtoms.ExternalFunctionProcessors
         }
     }
 
-    public class ExternalFunctionProcessorContextWithResult : ExternalFunctionProcessorContext
-    {
-        private readonly bool _wasQueueEmpty;
-        private object _returnValue;
-
-        public ExternalFunctionProcessorContextWithResult(InkAtomsStory source, object[] parameters)
-            : base(parameters)
-        {
-            _wasQueueEmpty = source.MainThreadQueue.IsEmpty;
-        }
-
-        public object returnValue
-        {
-            get => _returnValue;
-            set
-            {
-                if (!_wasQueueEmpty)
-                    throw new UnmanageableAsyncSequenceException(
-                        "Cannot execute an async function that returns a value after another async function; add a no-op '@' line between them.");
-
-                if (locked)
-                    throw new UnmanageableAsyncSequenceException(
-                        "Cannot set the result of an asynchronous operation after the first yield");
-
-                _returnValue = value;
-            }
-        }
-
-        private bool locked { get; set; }
-
-        internal void Lock()
-        {
-            locked = true;
-        }
-    }
+    
 }
---
+++ Runtime/ExternalFunctionProcessors/UnmanageableAsyncSequenceException.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace LemuRivolta.InkAtoms.ExternalFunctionProcessors
+{
+    [Serializable]
+    public class UnmanageableAsyncSequenceException : Exception
+    {
+        public UnmanageableAsyncSequenceException()
+        {
+        }
+
+        public UnmanageableAsyncSequenceException(string message) : base(message)
+        {
+        }
+
+        public UnmanageableAsyncSequenceException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected UnmanageableAsyncSequenceException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }}
