bb420d037a2769ecb1534e5d1897e86e89933944
mattia <mattia.belletti@gmail.com>
2024-06-29 14:58:52 +0200

    feat: external functions errors

---
+++ Packages/it.lemurivolta.ink-atoms/Runtime/ExternalFunctionProcessors/ExternalFunctionProcessorContextWithResult.cs
@@ -0,0 +1,37 @@
+namespace LemuRivolta.InkAtoms.ExternalFunctionProcessors
+{
+    public class ExternalFunctionProcessorContextWithResult : ExternalFunctionProcessorContext
+    {
+        private readonly bool _wasQueueEmpty;
+        private object _returnValue;
+
+        public ExternalFunctionProcessorContextWithResult(InkAtomsStory source, object[] parameters)
+            : base(parameters)
+        {
+            _wasQueueEmpty = source.MainThreadQueue.IsEmpty;
+        }
+
+        public object returnValue
+        {
+            get => _returnValue;
+            set
+            {
+                if (!_wasQueueEmpty)
+                    throw new UnmanageableAsyncSequenceException(
+                        "Cannot execute an async function that returns a value after another async function; add a no-op '@' line between them.");
+
+                if (locked)
+                    throw new UnmanageableAsyncSequenceException(
+                        "Cannot set the result of an asynchronous operation after the first yield");
+
+                _returnValue = value;
+            }
+        }
+
+        private bool locked { get; set; }
+
+        internal void Lock()
+        {
+            locked = true;
+        }
+    }}
